da1679da86c3676a80441f657345dc21e1b47d19
twiertzema <travis.wiertzema@gmail.com>
2017-11-17 22:57:04 -0600

    some UI/UX polish

--- src/components/cell.js
+++ src/components/cell.js
@@ -4,6 +4,20 @@ import classnames from "classnames";
 import { CELL_STATE } from "../logic/board";
 import styles from "./cell.css";
 
+const getCellTextColor = (mineCount) => {
+    switch (mineCount) {
+        case 1: return "#0000fe";
+        case 2: return "#017f01";
+        case 3: return "#fe0000";
+        case 4: return "#010080";
+        case 5: return "#810102";
+        case 6: return "#007f7e";
+        case 7: return "#fff";
+        case 8: return "#808080";
+    }
+    return "#0000";
+};
+
 /**
  * Expects the following props:
  * - x: number
@@ -47,8 +61,14 @@ export default class CellComponent extends React.Component {
                 content = "?";
                 break;
             case CELL_STATE.REVEALED:
-                content = hasMine ? 'X' : mineCount;
-                // TODO: Change font color based on mineCount.
+                if (hasMine) {
+                    // TODO: Show mine icon.
+                    content = "X";
+                } else {
+                    if (mineCount > 0) {
+                        content = <span style={{color: getCellTextColor(mineCount)}}>{mineCount}</span>;
+                    }
+                }
                 cellClassName = classnames(cellClassName, styles.revealed);
                 break;
             case CELL_STATE.DEFAULT:
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 
-import globalStyles from "./style.css";
+import globalStyles from "./index.css";
 
 import { mainReducer } from "./logic/board";
 
