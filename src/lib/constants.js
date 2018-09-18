3bc9d9b30028d3b61b6131164bca2b629b0313ba
twiertzema <4044268+twiertzema@users.noreply.github.com>
2018-09-17 23:48:22 -0500

    Way beefed up code doc, added documentation.yml to organize documentation.js page

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,20 +3,36 @@
  * @enum {number}
  */
 export const CELL_STATE = {
+  /** Initial state of all [Cells](Cell). Defined as the absence of all other state. */
   DEFAULT: 0,
+  /** The user has placed a flag on this {@link Cell}. Flagged [Cells](Cell) are not revealed when the user clicks on them. */
   FLAGGED: 1,
+  /** The user has revealed this {@link Cell}. Once a {@link Cell} is revealed, the user can no longer interact with a cell. */
   REVEALED: 2,
+  /** The user has marked this {@link Cell} with a question mark indicating that they are unsure whether or not this {@link Cell} contains a mine. */
   QUESTIONED: 4
 };
 
-/** @type Config */
-export const CONFIG_DEFAULT = { x: 0, y: 0, mines: 0 };
-
-/** @type Config */
+/**
+ * A 9×9 board with 10 mines.
+ * @type {Config}
+ */
 export const CONFIG_EASY = { x: 9, y: 9, mines: 10 };
 
-/** @type Config */
+/**
+ * A 16×16 board with 40 mines.
+ * @type {Config}
+ */
 export const CONFIG_INTERMEDIATE = { x: 16, y: 16, mines: 40 };
 
-/** @type Config */
+/**
+ * A 30×16 board with 99 mines.
+ * @type {Config}
+ */
 export const CONFIG_EXPERT = { x: 30, y: 16, mines: 99 };
+
+/**
+ * A 0×0 board with 0 mines. The initial state of a board before a different configuration is loaded.
+ * @type {Config}
+ */
+export const CONFIG_DEFAULT = { x: 0, y: 0, mines: 0 };
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,35 +1,57 @@
 import { CELL_STATE } from "./constants";
 
-/** Indicates that the supplied configuration is not valid. */
-export class InvalidConfigError extends Error {
-  constructor(...params) {
-    super("invalid confifg", ...params);
-
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, InvalidConfigError);
-    }
-  }
-}
-
-/** Indicates that the supplied coordinates are out of bounds for the given board. */
-export class OutOfBoundsError extends Error {
-  constructor(...params) {
-    super("out of bounds", ...params);
-
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, OutOfBoundsError);
-    }
-  }
-}
-
-/** @typedef {{x: number, y: number, mines: number}} Config */
-/** @typedef {{state: number, hasMine: boolean, mineCount: number}} Cell */
-/** @typedef {Cell[][]} Board */
+// TODO: Derive more information from boards instead of requiring config to always be passed in.
 
 /**
- * Verifies that the supplied configuration is valid.
+ * Configuration object that defines a board's setup.
+ *
+ * @typedef {{x: number, y: number, mines: number}} Config
+ * @property {number} x Number of mines on the X axis. *Note:* Must be >= `0`.
+ * @property {number} y Number of mines on the Y axis. *Note:* Must be >= `0`.
+ * @property {number} mines Number of mines contained on the board.
+ *  *Note:* Must adhere to the following rules:
+ *  - Must be >= `0`.
+ *  - Must be <= the product of `x` and `y`.
+ */
+/**
+ * 2-dimensional {@link Array} representing a board in Minesweeper.
+ *
+ * **Note:** The first order of [Arrays](Array) represents the rows of the
+ *  board, and the second order represents the columns. This means that the Y
+ *  coordinate of a given {@link Cell} is used as the index of the first order
+ *  and the X coordinate is used as the index of the second order.
+ *
+ * @typedef {Array<Array<Cell>>} Board
+ */
+/**
+ * Object representing a cell on the board. These comprise the makeup of the
+ *  {@link Board}.
+ *
+ * @typedef {{hasMine: boolean, mineCount: number, state: CELL_STATE}} Cell
+ * @property {boolean} hasMine Indicates whether or not this cell contains a
+ *  mine. Default value is `false`.
+ * @property {number} mineCount Indicates how many mines are adjacent to this
+ *  cell. Default value is `0`.
+ * @property {CELL_STATE} state Enum value indicating what state the cell is in.
+ *  Default value is `CELL_STATE.DEFAULT`.
+ */
+
+/**
+ * Verifies that the supplied {@link Config} is valid.
+ *
+ * @function
  * @param {Config} config
  * @returns {boolean}
+ * @example
+ * const myConfig = { x: 10, y: 15, mines: 100 };
+ * const isMyConfigValid = isConfigValid(myConfig);
+ * console.log(isMyConfigValid); // true
+ *
+ * console.log(isConfigValid(null)); // false
+ * console.log(isConfigValid({ x: 5 })); // false
+ * console.log(isConfigValid({ x: 5, y: 5 })); // false
+ * console.log(isConfigValid({ x: 5, y: 5, mines: 1337 })); // false
+ * console.log(isConfigValid({ x: "5", y: "5", mines: "13" })); // false
  */
 export const isConfigValid = config => {
   return (
@@ -49,11 +71,24 @@ export const isConfigValid = config => {
 };
 
 /**
+ * Determines if the supplied `x` and `y` coordinates are out of bounds for the
+ *  given {@link Config}.
+ *
+ * @function
  * @param {Config} config
  * @param {number} x
  * @param {number} y
  * @returns {boolean}
- * @throws {InvalidConfigError}
+ * @throws {InvalidConfigError} if `config` is not valid.
+ * @example
+ * const myConfig = { x: 10, y: 15, mines: 100 };
+ * console.log(isOutOfBounds(myConfig, 4, 0)); // false
+ *
+ * console.log(isOutOfBounds(myConfig, 107, 3)); // true
+ * console.log(isOutOfBounds(myConfig, 5, -1)); // true
+ * console.log(isOutOfBounds(myConfig, 10, 10)); // true
+ *
+ * isOutOfBounds(invalidConfig, 10, -1); // throws InvalidConfigError
  */
 export const isOutOfBounds = (config, x, y) => {
   if (!isConfigValid(config)) throw new InvalidConfigError(config);
@@ -61,10 +96,23 @@ export const isOutOfBounds = (config, x, y) => {
 };
 
 /**
- * Generates a new blank board based on the provided config.
+ * Generates the 2-dimensional {@link Array} representing the board using the
+ *  provided {@link Config}.
+ *
+ * **Note:** This does not place the mines on the board. For that, you must call
+ *  {@link placeMines}.
+ *
+ * @function
  * @param {Config} config
- * @returns {Board} New board.
- * @throws {InvalidConfigError}
+ * @returns {Board} New blank board.
+ * @throws {InvalidConfigError} if `config` is not valid.
+ * @example
+ * getBoard(CONFIG_EASY); // Returns a 9x9 2D Array of Cells in their default state.
+ *
+ * const myConfig = { x: 10, y: 15, mines: 100 };
+ * getBoard(myConfig); // Returns a 10x10 2D Array of Cells in their default state.
+ *
+ * getBoard(invalidConfig); // throws InvalidConfigError
  */
 export const getBoard = config => {
   if (!isConfigValid(config)) throw new InvalidConfigError(config);
@@ -91,28 +139,49 @@ export const getBoard = config => {
 };
 
 /**
- * @callback forEachCell
- * @param {Cell} cell
- * @param {number} x
- * @param {number} y
+ * The format for the `action` parameter of {@link forEachAdjacentCell}.
+ *
+ * @callback forEachAdjacentCellCallback
+ * @param {Cell} cell The current {@link Cell} in the iteration.
+ * @param {number} x X coordinate of the cell.
+ * @param {number} y Y coordinate of the cell.
  */
 /**
- * @param {Config} config
- * @param {Board} board
- * @param {number} x
- * @param {number} y
- * @param {forEachCell} action
- * @throws {InvalidConfigError}
- * @throws {OutOfBoundsError}
+ * Executes the `action` callback for every cell adjacent to the target `x` and
+ *  `y` coordinates (excluding out-of-bounds coordinates).
+ *
+ * @function
+ * @param {Config} config {@link Config} used to generate `board` (for reference).
+ * @param {Board} board {@link Board} from which to retrive the [Cells](Cell).
+ * @param {number} x X coordinate of the target {@link Cell}.
+ * @param {number} y Y coordinate of the target {@link Cell}.
+ * @param {forEachAdjacentCellCallback} action Callback function to be exectued
+ *  for every (valid) adjacent cell.
+ * @returns {void}
+ * @throws {InvalidConfigError} if `config` is not valid.
+ * @throws {OutOfBoundsError} if `x` and `y` indicate a cell that is out of
+ *  bounds for `config`.
+ * @example
+ * const myConfig = { x: 10, y: 15, mines: 100 };
+ * const myBoard = getBoard(myConfig);
+ * forEachAdjacentCell(myConfig, myBoard, 0, 5, (cell, x, y) => {
+ *   console.log({ x, y });
+ * });
+ * // Will output the following:
+ * //  { x: 0, y: 4 }
+ * //  { x: 0, y: 6 }
+ * //  { x: 1, y: 4 }
+ * //  { x: 1, y: 5 }
+ * //  { x: 1, y: 6 }
  */
 export const forEachAdjacentCell = (config, board, x, y, action) => {
   if (!isConfigValid(config)) throw new InvalidConfigError(config);
   if (isOutOfBounds(config, x, y)) throw new OutOfBoundsError(x, y);
 
-  for (let i = -1; i < 2; i++) {
-    for (let j = -1; j < 2; j++) {
-      const checkX = x + j;
-      const checkY = y + i;
+  for (let j = -1; j < 2; j++) {
+    for (let i = -1; i < 2; i++) {
+      const checkX = x + i;
+      const checkY = y + j;
 
       if (i === 0 && j === 0) continue; // Skip the specified cell.
       if (isOutOfBounds(config, checkX, checkY)) continue;
@@ -122,15 +191,56 @@ export const forEachAdjacentCell = (config, board, x, y, action) => {
 };
 
 /**
- * Modifies the given board by placing a mine at the specified coordinates
- * and incrementing the mineCount of adjacent cells.
- * @param {Config} config
- * @param {Board} board
- * @param {number} x
- * @param {number} y
+ * Modifies the given {@link Board} by setting `hasMine` to `true` for the
+ *  {@link Cell} at the specified `x` and `y` coordinates and increments
+ *  `mineCount` for all adjacent cells.
+ *
+ * This function is used by {@link placeMines} to populate a board.
+ *
+ * If the specified {@link Cell} already has a mine, this function does nothing
+ *  and returns `false`.
+ *
+ * **Warning:** This is <u>not</u> a pure function.
+ *
+ * @function
+ * @param {Config} config {@link Config} used to generate `board` (for reference).
+ * @param {Board} board {@link Board} on which to place the mine.
+ * @param {number} x X coordinate of the target {@link Cell}.
+ * @param {number} y Y coordinate of the target {@link Cell}.
  * @returns {boolean} `true` if a modification took place; `false` otherwise.
- * @throws {InvalidConfigError}
- * @throws {OutOfBoundsError}
+ * @throws {InvalidConfigError} if `config` is not valid.
+ * @throws {OutOfBoundsError} if `x` and `y` indicate a cell that is out of
+ *  bounds for `config`.
+ * @example
+ * const myConfig = { x: 10, y: 15, mines: 100 };
+ * const myBoard = getBoard(myConfig);
+ * console.log(myBoard[7][4].hasMine) // false
+ *
+ * placeMine(myConfig, myBoard, 4, 7)
+ * console.log(myBoard[7][4].hasMine) // true
+ *
+ * console.log(myBoard[6][3].mineCount) // 1
+ * console.log(myBoard[7][3].mineCount) // 1
+ * console.log(myBoard[8][3].mineCount) // 1
+ * console.log(myBoard[6][4].mineCount) // 1
+ * console.log(myBoard[8][4].mineCount) // 1
+ * console.log(myBoard[6][5].mineCount) // 1
+ * console.log(myBoard[7][5].mineCount) // 1
+ * console.log(myBoard[8][5].mineCount) // 1
+ *
+ * placeMine(myConfig, myBoard, 4, 5)
+ *
+ * console.log(myBoard[4][3].mineCount) // 1
+ * console.log(myBoard[5][3].mineCount) // 1
+ * console.log(myBoard[6][3].mineCount) // 2
+ * console.log(myBoard[4][4].mineCount) // 1
+ * console.log(myBoard[6][4].mineCount) // 2
+ * console.log(myBoard[4][5].mineCount) // 1
+ * console.log(myBoard[5][5].mineCount) // 1
+ * console.log(myBoard[6][5].mineCount) // 2
+ *
+ * placeMine(invalidConfig, [][], 0, 0) // throws InvalidConfigError
+ * placeMine(myConfig, myBoard, 100, 3) // throws OutOfBoundsError
  */
 export const placeMine = (config, board, x, y) => {
   if (!isConfigValid(config)) throw new InvalidConfigError(config);
@@ -150,17 +260,37 @@ export const placeMine = (config, board, x, y) => {
   return true;
 };
 
+// TODO: Rename so as not to be so similar to `placeMine`.
 /**
- * Randomly places mines on the given board, avoiding the specified seed coordinates.
- * @param {Config} config
- * @param {Board} board
- * @param {number} seedX
- * @param {number} seedY
+ * Randomly places mines on `board` (using {@link placeMine}), avoiding the
+ *  {@link Cell} specified by `seedX` and `seedY`.
+ *
+ * In practice, the board is not populated with mines until the user clicks the
+ *  first cell. This means that the first cell the user clicks can never be a
+ *  mine, and this is why {@link getBoard} and this function are separate.
+ *
+ * **Warning:** This is <u>not</u> a pure function.
+ *
+ * @function
+ * @param {Config} config {@link Config} used to generate `board` (for reference).
+ * @param {Board} board {@link Board} on which to place the mines.
+ * @param {number} seedX X coordinate of the seed {@link Cell}.
+ * @param {number} seedY Y coordinate of the seed {@link Cell}.
  * @returns {Board} Modified `board` with randomly placed mines.
- * @throws {InvalidConfigError}
- * @throws {OutOfBoundsError}
+ * @throws {InvalidConfigError} if `config` is not valid.
+ * @throws {OutOfBoundsError} if `seedX` and `seedY` indicate a cell that is out
+ *  of bounds for `config`.
+ * @example
+ * const myConfig = { x: 10, y: 15, mines: 100 }
+ * const myBoard = getBoard(myConfig)
+ *
+ * placeMines(myConfig, myBoard, 4, 7)
+ *
+ * // `myBoard` will now be randomly populated with mines and the cells'
+ * //  `mineCount` will be set.
  */
 export const placeMines = (config, board, seedX, seedY) => {
+  // TODO: Rename `seedX`/`seedY`; the names make it sound like this function is idempotent.
   if (!isConfigValid(config)) throw new InvalidConfigError(config);
   if (isOutOfBounds(config, seedX, seedY))
     throw new OutOfBoundsError(seedX, seedY);
@@ -190,14 +320,47 @@ export const placeMines = (config, board, seedX, seedY) => {
 };
 
 /**
- * Modifies the given board by revealing all empty and empty-adjacent cells
- * connected to the given coordinates.
- * @param {Config} config
- * @param {Board} board
- * @param {number} x
- * @param {number} y
- * @throws {InvalidConfigError}
- * @throws {OutOfBoundsError}
+ * If the origin {@link Cell} specified by `x` and `y` is "empty" (has a
+ *  `mineCount` of `0`), this function modifies `board` by revealing all empty
+ *  and empty-adjacent [Cells](Cell); otherwise, this function does nothing.
+ *
+ * **Warning:** This is <u>not</u> a pure function.
+ *
+ * @function
+ * @param {Config} config {@link Config} used to generate `board` (for reference).
+ * @param {Board} board {@link Board} for which to reveal the [Cells](Cell).
+ * @param {number} x X coordinate of the origin {@link Cell}.
+ * @param {number} y Y coordinate of the origin {@link Cell}.
+ * @returns {void}
+ * @throws {InvalidConfigError} if `config` is not valid.
+ * @throws {OutOfBoundsError} if `x` and `y` indicate a cell that is out of
+ *  bounds for `config`.
+ * @example
+ * const myConfig = { x: 5, y: 5, mines: 10 }
+ * const myBoard = getBoard(myConfig)
+ * placeMines(myConfig, myBoard, 0, 0)
+ *
+ * // Assume the following board (x = mine):
+ * //  1 1 0 0 0
+ * //  x 4 2 1 0
+ * //  x x x 4 2
+ * //  x x x x x
+ * //  x 4 3 3 2
+ *
+ * cascadeCells(myConfig, myBoard, 2, 0)
+ *
+ * // This will reveal the following cells (r = revealed):
+ * //  - r r r r
+ * //  - r r r r
+ * //  - - - r r
+ * //  - - - - -
+ * //  - - - - -
+ *
+ * cascadeCells(myConfig, myBoard, 1, 4)
+ * // Since the target cell would have a `minCount` of 4, this would do nothing.
+ *
+ * cascadeCells(invalidConfig, [][], 0, 0) // throws InvalidConfigError
+ * cascadeCells(myConfig, myBoard, 100, 100) // throws OutOfBoundsError
  */
 export const cascadeCells = (config, board, x, y) => {
   if (!isConfigValid(config)) throw new InvalidConfigError(config);
@@ -213,6 +376,7 @@ export const cascadeCells = (config, board, x, y) => {
  * @param {Board} board
  * @param {number} x
  * @param {number} y
+ * @returns {void}
  * @private
  */
 const _cascadeCells = (config, board, x, y) => {
@@ -222,3 +386,31 @@ const _cascadeCells = (config, board, x, y) => {
     if (cell.mineCount === 0) _cascadeCells(config, board, _x, _y);
   });
 };
+
+/**
+ * Indicates that the supplied configuration is not valid.
+ * Automatically supplies the error message of `"invalid config"`.
+ */
+export class InvalidConfigError extends Error {
+  constructor(...params) {
+    super("invalid confifg", ...params);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, InvalidConfigError);
+    }
+  }
+}
+
+/**
+ * Indicates that the supplied coordinates are out of bounds for the given board.
+ * Automatically supplies the error message of `"out of bounds"`.
+ */
+export class OutOfBoundsError extends Error {
+  constructor(...params) {
+    super("out of bounds", ...params);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, OutOfBoundsError);
+    }
+  }
+}
