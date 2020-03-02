5f76c6bb7f8e30e7388b564bd86c4678fd25b2e1
twiertzema <4044268+twiertzema@users.noreply.github.com>
2020-03-02 14:05:02 -0600

    Got it actually running! XD

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,4 @@ import React from "react";
 
 import Board from "./components/Board";
 
-export default () => (
-  <div>
-    <h1>Minesweeper!</h1>
-    <Board />
-  </div>
-);
+export default () => <Board />;
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./module-fix";
 import "./index.css";
 
 import App from "./App";
